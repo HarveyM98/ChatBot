965267f41c3b4dc2dbe34fd2f62c9a19be003bec
HarveyM98 <hsmc10n@gmail.com>
2023-02-07 15:17:27 -0500

    cambios 23

--- app.js
+++ app.js
@@ -10,28 +10,36 @@ const DBProvider = require('@bot-whatsapp/database/mock')
 
 const FlujoBebidas = addKeyword(['Bebidas','Bebida'])
 .addAnswer('*Elige la bebida que deseas comprar:*')
-.addAnswer('',{media:'https://licoreschullavida.com/wp-content/uploads/2020/11/switch-bongo-bongo.jpg'})
-.addAnswer('',{buttons:[
+.addAnswer('*1*',{buttons:[
     {
         body: 'Switch ($3,50)'
-    },
+    }
+]})
+.addAnswer(' ',{media:'https://licoreschullavida.com/wp-content/uploads/2020/11/switch-bongo-bongo.jpg'})
+.addAnswer('*2*',{buttons:[
     {
         body: 'Zhumir Naranjilla ($5,75)'
     }
 ]})
-
-/*    {
-        body: '*Zhumir Naranjilla* ($5,75)'
-    },
+.addAnswer(' ',{media:'https://www.supermercadosantamaria.com/documents/10180/10504/100757_M.jpg'})
+.addAnswer('*3*',{buttons:[
     {
-        body: '*Monster* ($3,50)'
-    },
+        body: 'Monster ($3,50)'
+    }
+]})
+.addAnswer(' ',{media:'https://www.fybeca.com/dw/image/v2/BDPM_PRD/on/demandware.static/-/Sites-masterCatalog_FybecaEcuador/default/dw99280eb3/images/large/119662-BEBIDA-MONSTER-ENERGY-473ML-LATA.jpg?sw=1000&sh=1000'})
+.addAnswer('*4*',{buttons:[
     {
-        body: '*Red Bull* ($2,50)'
-    },
+        body: 'Red Bull ($2,50)'
+    }
+]})
+.addAnswer(' ',{media:'https://www.fybeca.com/dw/image/v2/BDPM_PRD/on/demandware.static/-/Sites-masterCatalog_FybecaEcuador/default/dw6a7c8972/images/large/86375-ENERGIZANTE-RED-BULL-RED-BULL-250-ML-UNIDAD.jpg?sw=1000&sh=1000'})
+.addAnswer('*5*',{buttons:[
     {
-        body: '*Café Cafelixc ($5,00)*'
-    }*/
+        body: 'Café Cafelixc ($5)'
+    }
+]})
+.addAnswer(' ',{media:'https://www.supermercadosantamaria.com/documents/10180/10504/120401440_G.jpg'})
 
 const FlujoComprar = addKeyword(['Comprar','Compras'])
 .addAnswer('¿En qué categoría se encuentra tu compra?',
